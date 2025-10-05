d385a77daf249c620110d5dfbdc89e2957fb2390
Egor Seliunin <Egor.Seliunin@nielseniq.com>
2025-10-05 19:58:18 +0200

    feat: Refactor main function to load and transform data using _apply_halflife

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import pandas as pd
+from pathlib import Path
+from app.utils.transformations import _apply_halflife
+
 def main():
-    print("Hello from mle-tech-assessment-jun!")
+    data_path = Path(__file__).parent / "data" / "raw" / "raw_data.csv"
+    example_series = pd.read_csv(data_path, parse_dates=["date_week"])["tv_ad_executions"].values
+    halflife = 2.5
+    transformed_series = _apply_halflife(example_series, halflife)
+    print(transformed_series)
 
 
 if __name__ == "__main__":
